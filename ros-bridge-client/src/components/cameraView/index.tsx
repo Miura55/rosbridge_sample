c9def82ba60f578b1b887c6b52ee6d0b3d0371df
Miura55 <acordion.piano@gmail.com>
2023-02-25 14:58:26 +0000

    Revert "Add ros module"
    This reverts commit 4f24777bffb28c289a8d30e26223df9b2e41215d.

--- ros-bridge-client/src/components/cameraView/index.tsx
+++ ros-bridge-client/src/components/cameraView/index.tsx
@@ -1,13 +1,28 @@
 import { useState, useEffect } from 'react';
-import { imageTopic } from '../../services/subscriber';
+import ROSLIB from 'roslib';
 
 export const Camera = () => {
   const [imageData, setImageData] = useState('');
 
   useEffect(() => {
+    const ros = new ROSLIB.Ros({
+      url: 'ws://localhost:9090',
+    });
+
+    const imageTopic = new ROSLIB.Topic({
+      ros: ros,
+      name: '/head_camera/image_raw/compressed',
+      messageType: 'sensor_msgs/CompressedImage',
+    });
+
     imageTopic.subscribe((message: any) => {
       setImageData(`data:image/png;base64,${message.data}`);
     });
+
+    return () => {
+      imageTopic.unsubscribe();
+      ros.close();
+    };
   }, []);
 
   return (
